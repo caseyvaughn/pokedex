e1bba07af2e70800a57f06387a92a6f7ee20ec8c
soleilyasmina <mounlasolomon@gmail.com>
2020-11-23 10:11:21 -0500

    restructure to make sense

--- pokedex-starter/src/App.js
+++ pokedex-starter/src/App.js
@@ -7,20 +7,20 @@ function App() {
   return (
     <>
       <Navbar className="bg-dark d-flex justify-content-between px-5">
+        <Navbar.Brand className="d-flex align-items-center" href="https://pokeapi.co/">
+          <Image
+            src="https://www.freeiconspng.com/uploads/pokeball-transparent-png-2.png"
+            width="55"
+            height="55"
+            roundedCircle
+            className="d-inline-block align-top m-2 d-inline"
+             alt="Pokéball"
+          />
+           <Nav.Item className="text-white font-weight-bold">
+            Pokédex
+          </Nav.Item>
+        </Navbar.Brand>
         <Nav>
-          <Navbar.Brand className="d-flex align-items-center" href="https://pokeapi.co/">
-            <Image
-              src="https://www.freeiconspng.com/uploads/pokeball-transparent-png-2.png"
-              width="55"
-              height="55"
-              roundedCircle
-              className="d-inline-block align-top m-2 d-inline"
-              alt="Pokéball"
-            />
-            <Nav.Item className="text-white font-weight-bold">
-              Pokédex
-            </Nav.Item>
-          </Navbar.Brand>
         </Nav>
       </Navbar>
       <Container className="d-flex justify-content-center align-items-center" fluid>
